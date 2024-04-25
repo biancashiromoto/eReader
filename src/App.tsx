2900ac82af56728495d0532061cf94745e9d5b1a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-04-25 16:14:34 -0300

    refactor: Create function changeFontSize

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
 
   useEffect(() => {
     document.body.style.backgroundColor = config.isDarkModeOn ? "black" : "white";
+    document.body.style.color = config.isDarkModeOn ? "white" : "black";
   }, [config.isDarkModeOn])
 
   const toggleMode = () => {
@@ -18,41 +19,37 @@ function App() {
     }));
   }
 
+  const changeFontSize = (action: string) => {
+    setConfig((prevState) => ({
+      ...prevState,
+      fontSize: action === "increase" ? prevState.fontSize + 2 : prevState.fontSize - 2
+    }));
+  }
+
+
   return (
-    <div style={{backgroundColor: config.isDarkModeOn ? "black" : "white"}}>
-      <h1
-        style={{color: config.isDarkModeOn ? "white" : "black"}}
-      >E-reader</h1>
+    <div>
+      <h1>E-reader</h1>
       <article
       >
         <p
-          style={{fontSize:`${config.fontSize}px`, color: config.isDarkModeOn ? "white" : "black"}}
+          style={{fontSize:`${config.fontSize}px`}}
         >
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti culpa error tempore eligendi architecto earum delectus placeat quisquam. Rerum eveniet cum voluptas sapiente sed voluptates animi laborum impedit, quos ab.
         </p>
         <p
-          style={{fontSize:`${config.fontSize}px`, color: config.isDarkModeOn ? "white" : "black"}}
+          style={{fontSize:`${config.fontSize}px`}}
         >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis accusantium ad sapiente architecto voluptatem quas modi? Magnam officia nisi quis ratione sed! Dolores, quaerat! Atque perferendis voluptas blanditiis! Ab, similique.
         </p>
       </article>
       <button
-        onClick={() => {
-          setConfig((prevState) => ({
-            ...prevState,
-            fontSize: prevState.fontSize + 2
-          }));
-        }}
+        onClick={() => changeFontSize("increase")}
       >
         Increase font size
       </button>
       <button
-        onClick={() => {
-          setConfig((prevState) => ({
-            ...prevState,
-            fontSize: prevState.fontSize - 2
-          }));
-        }}
+        onClick={() => changeFontSize("decrease")}
       >
         Decrease font size
       </button>
