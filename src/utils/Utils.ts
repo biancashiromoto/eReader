0de470f6ec7123ff089a52b81dd2a0a0f26ec698
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-04-25 17:15:21 -0300

    refactor: Create Utils class and methods savePreferences and loadPreferences

---
+++ src/utils/Utils.ts
@@ -0,0 +1,21 @@
+import { configType } from "../types/Types";
+
+export default class Utils {
+
+  public documentStyle: CSSStyleDeclaration = document.body.style;
+
+  /**
+   * savePreferences
+preferences   */
+  public savePreferences(preferences: configType): void {
+    localStorage.setItem("config", JSON.stringify(preferences));
+  }
+
+  /**
+   * loadPreferences
+preferences: config   */
+  public loadPreferences(): configType {
+    const preferences = localStorage.getItem("config");
+    return preferences ? JSON.parse(preferences) : {fontSize: 18, isDarkModeOn: true};
+  }
+}
