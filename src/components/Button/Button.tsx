d28681f787b1525519adc983fe084c8316181ed3
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-04-26 19:13:42 -0300

    chore: Update component Button to take data-testid and conditional to hide label

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,15 @@
 import ButtonProps from "../../interfaces/Interfaces";
 
-const Button: React.FC<ButtonProps> = ({name, onClick, label, className}) => {
+const Button: React.FC<ButtonProps> = ({name, onClick, label, className, "data-testid": testId}) => {
   return (
     <button
       className={className}
       name={name}
       onClick={onClick}
       type="button"
+      data-testid={testId}
     >
-      {label}
+      {label === "Toggle mode" ? "" : label}
     </button>
   )
 }
