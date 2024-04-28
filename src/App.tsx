6c93a27dbf9237ff862e7aa2b7d769ecf3914f8f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-04-28 01:41:55 -0300

    chore: Move font size management to component Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Utils from "./utils/Utils";
 import { configType } from "./types/Types";
-import Button from "./components/Button/Button";
 import AppContext from "./context/AppContext";
 import Header from "./components/Header/Header";
 
@@ -28,16 +27,6 @@ function App() {
     utils.toggleMode(config.isDarkModeOn);
     utils.savePreferences(config);
   }, [config])
-
-  // Resizes the text font to a new size passed as parameter.
-  const resizeFont = (newSize: number) => {
-    if (utils.validateFontSize(newSize)) {
-      setConfig({
-        ...config,
-        fontSize: newSize
-      })
-    }
-  }
 
   return (
     <AppContext.Provider
@@ -67,40 +56,6 @@ function App() {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur incidunt officia harum voluptatibus laboriosam consectetur qui itaque optio ut quaerat! Placeat odit sapiente voluptatibus quos, culpa sunt deleniti consequuntur id!
           </p>
         </article>
-        <div className="buttons-container">
-          <div className="font-size-buttons">
-            <Button
-              className="decrease-font-size-button font-size-button"
-              name="decrease-font-size"
-              label="-"
-              onClick={() => resizeFont(config.fontSize - 1)}
-              aria-label="Decrease font size"
-              role="button"
-            />
-            <label
-              htmlFor="font-size-range"
-              className="font-size-range"
-            >
-              {`Font size: ${config.fontSize}px`}
-              <input
-                id="font-size-range"
-                type="range"
-                value={config.fontSize}
-                max={utils.validFontSizes.max}
-                min={utils.validFontSizes.min}
-                onChange={(e) => resizeFont(e.target.valueAsNumber)}
-                aria-label={`Font Size: ${config.fontSize}px`}
-              />
-            </label>
-            <Button
-              className="increase-font-size-button font-size-button"
-              label="+"
-              name="increase-font-size"
-              onClick={() => resizeFont(config.fontSize + 1)}aria-label="Increase font size"
-              role="button"
-            />
-          </div>
-        </div>
       </div>
     </AppContext.Provider>
   )
