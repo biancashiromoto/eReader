4d04cf8073e6cbd547b47b048382e89bd9cd8317
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-04-29 15:39:49 -0300

    chore: Create text.ts

---
+++ src/helpers/text.ts
@@ -0,0 +1,6 @@
+export const text: string[] = [
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti culpa error tempore eligendi architecto earum delectus placeat quisquam. Rerum eveniet cum voluptas sapiente sed voluptates animi laborum impedit, quos ab.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis accusantium ad sapiente architecto voluptatem quas modi? Magnam officia nisi quis ratione sed! Dolores, quaerat! Atque perferendis voluptas blanditiis! Ab, similique.",
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur incidunt officia harum voluptatibus laboriosam consectetur qui itaque optio ut quaerat! Placeat odit sapiente voluptatibus quos, culpa sunt deleniti consequuntur id!",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur aperiam qui neque! Culpa cupiditate dicta modi? Debitis fugit unde, architecto harum aperiam, doloremque corporis alias eum accusamus ipsa quas saepe."
+]
